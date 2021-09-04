71368a0cedb7a6e4455cd4c62339a819c45ac45a
prabhakar.webdeveloper@gmail.com <sam@0521>
2021-09-04 22:31:00 +0530

    chagnes added

--- packages/container/config/webpack.prod.js
+++ packages/container/config/webpack.prod.js
@@ -8,7 +8,6 @@ const prodConfig = {
   mode: 'production',
   output: {
     filename: '[name].[contenthash].js',
-    publicPath: '/container/latest/'
   },
   plugins: [
     new ModuleFederationPlugin({
