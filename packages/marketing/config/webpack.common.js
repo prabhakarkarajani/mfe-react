7ea649f6b0def0d06efe407743a52a0bc30a9d6e
prabhakar.webdeveloper@gmail.com <sam@0521>
2021-09-04 14:42:10 +0530

    Created container deploy script

--- packages/marketing/config/webpack.common.js
+++ packages/marketing/config/webpack.common.js
@@ -1,3 +1,4 @@
+const HtmlWepackPlugin = require('html-webpack-plugin');
 module.exports = {
   module:{
     rules:[
@@ -13,5 +14,10 @@ module.exports = {
         },
       }
     ]
-  }
+  },
+  plugins:[
+    new HtmlWepackPlugin({
+      template: './public/index.html'
+    }),
+  ]
 }
--- packages/marketing/config/webpack.dev.js
+++ packages/marketing/config/webpack.dev.js
@@ -1,5 +1,5 @@
 const { merge } = require('webpack-merge');
-const HtmlWepackPlugin = require('html-webpack-plugin');
+
 const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
 const commonConfig = require('./webpack.common');
 const packageJson = require('../package.json');
@@ -20,10 +20,7 @@ const devConfig = {
       },
       shared: packageJson.dependencies,
     }),
-    new HtmlWepackPlugin({
-      template: './public/index.html'
-    }),
-    
+   
   ]
 };
 
