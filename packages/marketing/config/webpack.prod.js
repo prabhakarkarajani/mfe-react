30b3e0e64cad5d2da561427a7dfbdda4cbad9692
prabhakar.webdeveloper@gmail.com <sam@0521>
2021-09-06 00:03:35 +0530

    Added marketing deployment

--- packages/marketing/config/webpack.prod.js
+++ packages/marketing/config/webpack.prod.js
@@ -7,11 +7,13 @@ const packageJson = require('../package.json');
 const prodConfig = {
   mode: 'production',
   output: {
-    filename: '[name].[contenthash].js'
+    filename: '[name].[contenthash].js',
+    publicPath: '/marketing/latest/',
   },
   plugins:[
     new ModuleFederationPlugin({
       name: 'marketing',
+      filename: 'remoteEntry.js',
       exposes: {
         './MarketingApp': './src/bootstrap'
       },
